b23f702fb0a0667929027b6eb7ed87ae094364cb
KeisukeKudo <6696989+KeisukeKudo@users.noreply.github.com>
2020-07-04 20:39:29 +0900

    update: Add items required for OAuth to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,16 +11,20 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', static function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('provider_id')->nullable();
+            $table->string('provider_name')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->unique(['provider_id', 'provider_name']);
         });
     }
 
@@ -29,7 +33,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
